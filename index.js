b440315a8a42000e5c848960ca5776782c3e5a14
maniack <osef@osef.osef>
2020-01-06 15:18:09 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var FS = require('fs');
+const puppeteer = require("puppeteer-extra");
+const pluginStealth = require("puppeteer-extra-plugin-stealth")
+puppeteer.use(pluginStealth())
+
+const SHOWBROWSER = true;
+const ChromeUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36";
+
+puppeteer.launch({ headless: !SHOWBROWSER }).then(async browser => {
+    const page = await browser.newPage()
+    await page.setViewport({ width: 1024, height: 1080 })
+    await page.setUserAgent(ChromeUserAgent);
+    await page.goto('https://www.google.com/');
+    await page.waitFor(5000);
+});
